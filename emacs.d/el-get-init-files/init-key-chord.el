5d2f18b23db6e3855407972b9f4e8bd8f6fb8147
ochko <ochkol@reallyenglish.com>
2014-07-29 15:35:28 +0900

    update key chords for ace jumps

--- emacs.d/el-get-init-files/init-key-chord.el
+++ emacs.d/el-get-init-files/init-key-chord.el
@@ -1,7 +1,9 @@
 (key-chord-mode 1)
 
 (key-chord-define-global "ff" 'ido-find-file)
-(key-chord-define-global "j;" 'ace-jump-mode)
+(key-chord-define-global "j;" 'ace-jump-char-mode)
+(key-chord-define-global "jk" 'ace-jump-word-mode)
+(key-chord-define-global "jl" 'ace-jump-line-mode)
 (key-chord-define-global "fr" 'switch-to-buffer)
 (key-chord-define-global "gs" 'magit-status)
 
--- emacs.d/el-get-init-files/init-space-chord.el
+++ emacs.d/el-get-init-files/init-space-chord.el
@@ -1,6 +1,8 @@
 (key-chord-mode 1)
 
 (space-chord-define-global "f" 'ido-find-file)
-(space-chord-define-global "j" 'ace-jump-mode)
+(space-chord-define-global "j" 'ace-jump-char-mode)
+(space-chord-define-global "w" 'ace-jump-word-mode)
+(space-chord-define-global "l" 'ace-jump-line-mode)
 (space-chord-define-global "b" 'switch-to-buffer)
 (space-chord-define-global "g" 'magit-status)
