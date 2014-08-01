e88b998442ab2795a8502a288da4b6eab1bda29f
ochko <ochkol@reallyenglish.com>
2014-08-01 14:49:17 +0900

    comments on flycheck initializer

--- emacs.d/el-get-init-files/init-flycheck.el
+++ emacs.d/el-get-init-files/init-flycheck.el
@@ -1,1 +1,32 @@
 (setq flycheck-jshintrc "~/.jshintrc")
+
+;; Language Supports
+
+;; Ruby
+;; gem install rubocop ruby-lint
+(setq flycheck-rubocop-lint-only t)
+
+;; CSS
+;; npm install -g csslint
+
+;; Handlebars
+;; npm install -g handlebars
+
+;; Javascript
+;; npm install -g eslint
+;; npm install -g jshint
+
+;; JSON
+;; npm install -g jsonlint
+
+;; LESS
+;; npm install -g less
+
+;; Slim
+;; gem install slim
+
+;; Yaml
+;; npm install -g js-yaml
+
+;; Python
+;; pip install flake8
