64a56f21b719381b4dc9ff38f0b562a0017a0ddd
ochko <ochkol@reallyenglish.com>
2014-08-14 17:00:28 +0900

    ff an fr are frequent enough combinations

--- emacs.d/el-get-init-files/init-key-chord.el
+++ emacs.d/el-get-init-files/init-key-chord.el
@@ -1,10 +1,10 @@
 (key-chord-mode 1)
 
-(key-chord-define-global "ff" 'ido-find-file)
+(key-chord-define-global "fp" 'ido-find-file)
 (key-chord-define-global "j;" 'ace-jump-char-mode)
 (key-chord-define-global "jk" 'ace-jump-word-mode)
 (key-chord-define-global "jl" 'ace-jump-line-mode)
-(key-chord-define-global "fr" 'switch-to-buffer)
+(key-chord-define-global "bf" 'switch-to-buffer)
 (key-chord-define-global "gs" 'magit-status)
 
 (key-chord-define-global ";;" "\C-e;")
