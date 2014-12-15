1312603d5704b78104f755bf63c4cbcd3be13fdb
ochko <ochkol@reallyenglish.com>
2014-12-15 14:55:59 +0900

    my mother tongue is mn

--- emacs.d/el-get-init-files/init-langtool.el
+++ emacs.d/el-get-init-files/init-langtool.el
@@ -1,5 +1,4 @@
 (setq langtool-language-tool-jar "/usr/local/Cellar/languagetool/2.6/libexec/languagetool-commandline.jar"
-      langtool-mother-tongue "en"
       langtool-disabled-rules '("WHITESPACE_RULE"
                                 "EN_UNPAIRED_BRACKETS"
                                 "COMMA_PARENTHESIS_WHITESPACE"
