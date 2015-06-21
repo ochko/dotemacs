df37e34643193a1ad53ff83b652138e2026d847b
ochko <ochkol@reallyenglish.com>
2015-06-21 22:38:34 +0900

    markdown with markdoc

--- emacs.d/el-get-init-files/init-markdown-mode.el
+++ emacs.d/el-get-init-files/init-markdown-mode.el
@@ -2,4 +2,7 @@
 (add-to-list 'auto-mode-alist '("\\.text$" . markdown-mode))
 
 ;; Install Docter to use preview(https://github.com/alampros/Docter)
-(setq markdown-command "gfm")
+;; (setq markdown-command "gfm")
+
+;; Install markdoc from https://github.com/ochko/markdoc
+(setq markdown-command "markdoc")
