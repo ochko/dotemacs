11179a3372346d121a5f577457d2220d727bb665
ochko <ochkol@reallyenglish.com>
2015-11-11 17:48:17 +0900

    re-order el-get init file

--- emacs.d/elisp/el-get-initializer.el
+++ emacs.d/elisp/el-get-initializer.el
@@ -6,69 +6,83 @@
 ;;  (lambda (s)
 ;;    (goto-char (point-max))
 ;;    (eval-print-last-sexp)))
+;;
+;; Could not create connection to raw.github.com:443
+;; Evaluate:
+;; (el-get-invalidate-autoloads)
 
 (let ((el-get-install-dir (emacs-user-dir-concat "el-get/el-get"))
       (el-get-user-recipes (emacs-user-dir-concat "el-get-recipes"))
       (el-get-user-inits (emacs-user-dir-concat "el-get-init-files"))
       (el-get-user-packages
-       '(
+       '(ace-jump-mode
+         actionscript-mode
          ag
-         ace-jump-mode
-         smart-tab
+         alchemist
          auto-complete
-         smex
-         expand-region
-         multiple-cursors
-         visual-regexp-steroids
-         guide-key
-         key-chord
-         idomenu
-         flx
-         ido-vertical-mode
-         paredit
-         helm
-         thingatpt+
-         magit
-         projectile
+         coffee-mode
+         color-theme
+         deft
+         dired-details+
+         elixir
          emacs-dirtree
-         multi-term
-         rbenv
-         pbcopypaste
-         mo-git-blame
-         yasnippet
+         ember-mode
+         ensime
+         erlang-mode
+         expand-region
+         feature-mode
+         flx
          flycheck
+         guide-key
+         haml-mode
+         helm
+         ido-vertical-mode
+         idomenu
          js2-mode
          js2-refactor
-         nodejs-repl
-         yeoman-backbone
-         rspec-mode
-         rinari
-         ember-mode
-         org-mode
-         deft
-         coffee-mode
-         yaml-mode
-         haml-mode
-         slim-mode
-         markdown-mode
-         php-mode
-         geben
-         actionscript-mode
-         feature-mode
-         ensime
-         dired-details+
-         color-theme
-         solarized-emacs
+         key-chord
          mac-app-binding
-         langtool
-         writegood-mode
-         auctex
-         )))
+         magit
+         markdown-mode
+         mo-git-blame
+         multi-term
+         multiple-cursors
+         nodejs-repl
+         org-mode
+         paredit
+         pbcopypaste
+         php-mode
+         projectile
+         projectile-rails
+         rbenv
+         rinari
+         rspec-mode
+         sass-mode
+         slim-mode
+         smart-tab
+         smex
+         solarized-emacs
+         thingatpt+
+         visual-regexp-steroids
+         yaml-mode
+         yasnippet
+         yeoman-backbone)))
+
+  (add-to-list 'load-path el-get-install-dir)
+
+  (unless (require 'el-get nil 'noerror)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+
   (when (file-directory-p el-get-install-dir)
-    (add-to-list 'load-path el-get-install-dir)
-    (require 'el-get)
+
     (add-to-list 'el-get-recipe-path el-get-user-recipes)
+
     (setq el-get-user-package-directory el-get-user-inits)
+
     (el-get 'sync el-get-user-packages)))
 
 (provide 'el-get-initializer)
